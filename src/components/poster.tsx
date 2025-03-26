b1b043d70b4b5a7c7d9f12014553be428148ee4d
tankhang1506 <162155633+tankhang1506@users.noreply.github.com>
2025-03-26 20:43:13 +0700

    Add files via upload

---
+++ src/components/poster.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Poster = () => {
+  return (
+    <div className="w-full h-48 bg-gray-700 flex items-center justify-center text-white text-2xl font-bold">
+      Danh mục sản phẩm
+    </div>
+  );
+};
+
+export default Poster;
